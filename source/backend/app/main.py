f262141c1008146f72ebbfd6e98294a740ad4f11
neon <neon@gmail.com>
2026-08-18 04:59:18 +0330

    try fix error cors

--- source/backend/app/main.py
+++ source/backend/app/main.py
@@ -14,12 +14,8 @@ app = FastAPI(title=settings.APP_NAME, version="2.0.0", debug=settings.DEBUG)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://sales-analytics.vercel.app",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
